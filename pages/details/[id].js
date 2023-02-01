94617d999020c8a4d1cc2f670342898762782926
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-02-01 22:39:15 +0100

    react player remove

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -8,7 +8,6 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Footer from "../../comps/Footer";
-import ReactPlayer from "react-player";
 import Head from "next/head";
 
 const animedetails = ({ anime }) => {
